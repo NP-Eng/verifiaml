3425ba421536ab88800edad01ae769d100c1c156
Antonio Mejías Gil <anmegi.95@gmail.com>
2024-01-31 09:02:11 +0100

    perceptron test compiling

--- src/model/examples/simple_perceptron_mnist/mod.rs
+++ src/model/examples/simple_perceptron_mnist/mod.rs
@@ -58,27 +58,29 @@ mod tests {
     use ark_crypto_primitives::sponge::poseidon::PoseidonSponge;    
     use ark_bn254::{Fr, G1Affine};
     use ark_poly_commit::hyrax::HyraxPC;
+
+    use crate::model::Poly;
     
     type Sponge = PoseidonSponge<Fr>;
-    type Hyrax254 = HyraxPC<G1Affine, DenseMultilinearExtension<Fr>, Sponge>;
+    type Hyrax254 = HyraxPC<G1Affine, Poly<Fr>, Sponge>;
     
     use super::*;
 
     #[test]
     fn run_simple_perceptron_mnist() {
 
-        let dummy_input = vec![vec![30; 28]; 28];
-
-        let input: Vec<u8> = vec![];
+        let input = vec![vec![30; 28]; 28];
         let expected_output: Vec<u8> = vec![];
 
         let perceptron = build_simple_perceptron_mnist::<Fr, Sponge, Hyrax254>();
         
-        let input_i8 = input.into_iter().map(|x| ((x as i32) - 128) as i8).collect();
+        let input_i8 = input.into_iter().map(|r|
+            r.into_iter().map(|x| ((x as i32) - 128) as i8).collect::<Vec<i8>>()
+        ).collect::<Vec<Vec<i8>>>().into();
 
         let output = perceptron.evaluate(input_i8);
 
-        let output_u8 = output.iter().map(|x| ((x as i32) + 128) as u8).collect();
+        let output_u8: Vec<u8> = output.values()[0][0].iter().map(|x| ((*x as i32) + 128) as u8).collect();
 
         // assert_eq!(output_u8, expected_output);
         println!("Output: {:?}", output_u8);
