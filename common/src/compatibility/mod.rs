bf2af266a055069e02f46897f1c79a41acf96200
Cesar199999 <cesar.descalzo2@gmail.com>
2024-04-08 10:51:49 +0200

    Speed up compatibility tests by sharing Python model across inference executions

--- common/src/compatibility/mod.rs
+++ common/src/compatibility/mod.rs
@@ -25,7 +25,7 @@ mod tests {
     use ark_crypto_primitives::sponge::poseidon::PoseidonSponge;
     use more_asserts::*;
 
-    use pyo3::prelude::*;
+    use pyo3::{prelude::*, PyAny};
 
     const PERCEPTRON_PATH: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
@@ -39,33 +39,37 @@ mod tests {
     // within the allowed error.
     const ALLOWED_ERROR_MARGIN: f32 = 0.1;
 
-    fn get_model_input(model_name: &str, index: Option<usize>) -> QArray<f32> {
-        let model_input = Python::with_gil(|py| {
-            let perceptron: Py<PyAny> = PyModule::from_code(py, PERCEPTRON_PATH, "", "")
-                .unwrap()
-                .getattr("get_model_input")
-                .unwrap()
-                .into();
-            let result = perceptron.call1(py, (model_name, index.unwrap_or(150)));
+    fn run_python<F, K>(f: F) -> K
+    where
+        F: FnOnce(&Python) -> K,
+    {
+        Python::with_gil(|py| f(&py))
+    }
 
-            // Downcast the result to the expected type
-            result.unwrap().extract::<Vec<Vec<f32>>>(py).unwrap()
-        });
+    fn get_model(py: &Python, model_name: &str) -> Py<PyAny> {
+        let func: Py<PyAny> = PyModule::from_code(*py, PERCEPTRON_PATH, "", "")
+            .unwrap()
+            .getattr("get_model")
+            .unwrap()
+            .into();
+        func.call1(*py, (model_name,)).unwrap()
+    }
+
+    fn get_model_input(py: &Python, model: &Py<PyAny>, index: Option<usize>) -> QArray<f32> {
+        let result = model.call_method1(*py, "get_input", (index.unwrap_or(150),));
+
+        // Downcast the result to the expected type
+        let model_input = result.unwrap().extract::<Vec<Vec<f32>>>(*py).unwrap();
+
         QArray::from(model_input)
     }
 
-    fn get_model_output(model_name: &str, index: Option<usize>) -> QArray<u8> {
-        let model_output = Python::with_gil(|py| {
-            let perceptron: Py<PyAny> = PyModule::from_code(py, PERCEPTRON_PATH, "", "")
-                .unwrap()
-                .getattr("get_model_output")
-                .unwrap()
-                .into();
-            let result = perceptron.call1(py, (model_name, index.unwrap_or(150)));
+    fn get_model_output(py: &Python, model: &Py<PyAny>, index: Option<usize>) -> QArray<u8> {
+        let result = model.call_method1(*py, "get_output", (index.unwrap_or(150),));
 
-            // Downcast the result to the expected type
-            result.unwrap().extract::<Vec<u8>>(py).unwrap()
-        });
+        // Downcast the result to the expected type
+        let model_output = result.unwrap().extract::<Vec<u8>>(*py).unwrap();
+
         QArray::from(model_output)
     }
 
@@ -87,26 +91,22 @@ mod tests {
     }
 
     #[test]
-    fn test_two_layer_perceptron_mnist_single_input() {
-        let expected_input =
-            QArray::read("examples/two_layer_perceptron_mnist/data/input_test_150.json");
-        assert_eq!(get_model_input("QTwoLayerPerceptron", None), expected_input);
-    }
-
-    #[test]
     fn test_simple_perceptron_mnist_single_input() {
         let expected_input =
             QArray::read("examples/simple_perceptron_mnist/data/input_test_150.json");
-        assert_eq!(get_model_input("QSimplePerceptron", None), expected_input);
+        assert_eq!(
+            run_python(|py| { get_model_input(py, &get_model(py, "QSimplePerceptron"), None) }),
+            expected_input
+        );
     }
 
     #[test]
-    fn test_two_layer_perceptron_mnist_single_output() {
-        let expected_output =
-            QArray::read("examples/two_layer_perceptron_mnist/data/output_test_150.json");
+    fn test_two_layer_perceptron_mnist_single_input() {
+        let expected_input =
+            QArray::read("examples/two_layer_perceptron_mnist/data/input_test_150.json");
         assert_eq!(
-            get_model_output("QTwoLayerPerceptron", None),
-            expected_output
+            run_python(|py| { get_model_input(py, &get_model(py, "QTwoLayerPerceptron"), None) }),
+            expected_input
         );
     }
 
@@ -114,7 +114,20 @@ mod tests {
     fn test_simple_perceptron_mnist_single_output() {
         let expected_output =
             QArray::read("examples/simple_perceptron_mnist/data/output_test_150.json");
-        assert_eq!(get_model_output("QSimplePerceptron", None), expected_output);
+        assert_eq!(
+            run_python(|py| get_model_output(py, &get_model(py, "QSimplePerceptron"), None)),
+            expected_output
+        );
+    }
+
+    #[test]
+    fn test_two_layer_perceptron_mnist_single_output() {
+        let expected_output =
+            QArray::read("examples/two_layer_perceptron_mnist/data/output_test_150.json");
+        assert_eq!(
+            run_python(|py| get_model_output(py, &get_model(py, "QTwoLayerPerceptron"), None)),
+            expected_output
+        );
     }
 
     #[test]
@@ -122,24 +135,27 @@ mod tests {
         let two_layer_perceptron_mnist =
             build_two_layer_perceptron_mnist::<Fr, PoseidonSponge<Fr>, Ligero<Fr>>();
 
-        let correct_samples: usize = (0..NB_OUTPUTS)
-            .into_iter()
-            .map(|i| {
-                let raw_input = get_model_input("QTwoLayerPerceptron", Some(i));
-                let expected_output = get_model_output("QTwoLayerPerceptron", Some(i));
+        let correct_samples: usize = run_python(|py| {
+            let tf_lite_model = get_model(py, "QTwoLayerPerceptron");
+            (0..NB_OUTPUTS)
+                .into_iter()
+                .map(|i| {
+                    let raw_input = get_model_input(py, &tf_lite_model, Some(i));
+                    let expected_output = get_model_output(py, &tf_lite_model, Some(i));
 
-                let output = unpadded_inference(
-                    raw_input,
-                    &two_layer_perceptron_mnist,
-                    (
-                        S_INPUT_TWO_LAYER_PERCEPTRON_MNIST,
-                        Z_INPUT_TWO_LAYER_PERCEPTRON_MNIST,
-                    ),
-                );
+                    let output = unpadded_inference(
+                        raw_input,
+                        &two_layer_perceptron_mnist,
+                        (
+                            S_INPUT_TWO_LAYER_PERCEPTRON_MNIST,
+                            Z_INPUT_TWO_LAYER_PERCEPTRON_MNIST,
+                        ),
+                    );
 
-                (output == expected_output) as usize
-            })
-            .sum();
+                    (output == expected_output) as usize
+                })
+                .sum()
+        });
 
         println!(
             "Two-layer perceptron discrepancies: {} out of {}",
@@ -158,24 +174,27 @@ mod tests {
         let simple_perceptron_mnist =
             build_simple_perceptron_mnist::<Fr, PoseidonSponge<Fr>, Ligero<Fr>>();
 
-        let correct_samples: usize = (0..NB_OUTPUTS)
-            .into_iter()
-            .map(|i| {
-                let raw_input = get_model_input("QSimplePerceptron", Some(i));
-                let expected_output = get_model_output("QSimplePerceptron", Some(i));
+        let correct_samples: usize = run_python(|py| {
+            let tf_lite_model = get_model(py, "QSimplePerceptron");
+            (0..NB_OUTPUTS)
+                .into_iter()
+                .map(|i| {
+                    let raw_input = get_model_input(py, &tf_lite_model, Some(i));
+                    let expected_output = get_model_output(py, &tf_lite_model, Some(i));
 
-                let output = unpadded_inference(
-                    raw_input,
-                    &simple_perceptron_mnist,
-                    (
-                        S_INPUT_SIMPLE_PERCEPTRON_MNIST,
-                        Z_INPUT_SIMPLE_PERCEPTRON_MNIST,
-                    ),
-                );
+                    let output = unpadded_inference(
+                        raw_input,
+                        &simple_perceptron_mnist,
+                        (
+                            S_INPUT_SIMPLE_PERCEPTRON_MNIST,
+                            Z_INPUT_SIMPLE_PERCEPTRON_MNIST,
+                        ),
+                    );
 
-                (output == expected_output) as usize
-            })
-            .sum();
+                    (output == expected_output) as usize
+                })
+                .sum()
+        });
 
         println!(
             "Simple perceptron discrepancies: {} out of {}",
