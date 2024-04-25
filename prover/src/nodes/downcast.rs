a090acba79291cd76ee2fa41a6c3976da584bdb8
Cesar199999 <cesar.descalzo2@gmail.com>
2024-04-25 18:27:21 +0200

    Dirty commit

--- prover/src/nodes/downcast.rs
+++ prover/src/nodes/downcast.rs
@@ -12,9 +12,10 @@ use ark_std::any::Any;
 use ark_crypto_primitives::sponge::{Absorb, CryptographicSponge};
 use ark_ff::PrimeField;
 use ark_poly_commit::{LabeledCommitment, PolynomialCommitment};
+use rayon::vec;
 use crate::NodeOpsProve;
 
-pub trait DowncastNode<F, S, PCS, N, ST> 
+/* pub trait DowncastNode<F, S, PCS, N, ST> 
 where 
     F: PrimeField + Absorb,
     S: CryptographicSponge,
@@ -28,7 +29,7 @@ where
 
 macro_rules! downcast_node_impl {
     ($st:ty) => {
-        impl<F, S, PCS, N> DowncastNode<F, S, PCS, N, $st> for Box<N>
+        impl<F, S, PCS, N> DowncastNode<F, S, PCS, N, $st> for N
         where
             F: PrimeField + Absorb,
             S: CryptographicSponge,
@@ -51,12 +52,45 @@ macro_rules! downcast_node_impl {
     };
 }
 
-downcast_node_impl!(i8);
+downcast_node_impl!(i8); */
+
+// error[E0605]: non-primitive cast: `&dyn NodeOpsNative<i8>` as `&(dyn Any + 'static)`
+fn node_downcast<
+    F: PrimeField + Absorb,
+    S: CryptographicSponge,
+    PCS: PolynomialCommitment<F, Poly<F>, S>,
+>(node: impl NodeOpsNative<i8> + 'static) -> Box<dyn NodeOpsProve<F, S, PCS, i8>> {
+    let node = &node as &dyn Any;
+
+    match node.downcast_ref::<ReLUNode<i8>>() {
+        Some(s) => return Box::new((*s).clone()),
+        _ => {}
+    }
+
+    match node.downcast_ref::<ReshapeNode>() {
+        Some(s) => return Box::new((*s).clone()),
+        _ => {}
+    }
+
+    panic!("No implementor of NodeOpsProve was received");
+}
 
 
 #[test]
 fn test_downcast_relu() {
-    let node: Box<dyn NodeOpsNative<i8>> = Box::new(ReLUNode::new(1, 1));
-    let prove_node: &dyn NodeOpsProve<Fr, PoseidonSponge<Fr>, Ligero<Fr>, i8> = node.downcast();
-    assert_eq!(prove_node.type_name(), "ReLU");
+    let node1 = ReLUNode::new(1, 1);
+    let node2 = ReshapeNode::new(vec![1, 2], vec![1, 2]);
+
+    let node1_proof = node_downcast::<Fr, PoseidonSponge<Fr>, Ligero<Fr>>(node1);
+    let node2_proof = node_downcast::<Fr, PoseidonSponge<Fr>, Ligero<Fr>>(node2);
+
+    let nodes: Vec<Box<dyn NodeOpsNative<i8>>> = vec![Box::new(node1), Box::new(node2)];
+    let nodes_proof: Vec<Box<dyn NodeOpsProve<Fr, PoseidonSponge<Fr>, Ligero<Fr>, i8>>> = nodes
+    .into_iter()
+    .map(|x| node_downcast::<Fr, PoseidonSponge<Fr>, Ligero<Fr>>(*(x.as_ref())))
+    .collect();
+
+    println!("Node 1: {}", node1_proof.type_name());
+    println!("Node 2: {}", node2_proof.type_name());
+
 }
