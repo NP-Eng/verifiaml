aae5baa34b77396322014921ab1b8e39dd1b435f
Cesar199999 <cesar.descalzo2@gmail.com>
2024-04-26 11:56:54 +0200

    Hide downcast tests under test-types feature

--- prover/src/nodes/downcast.rs
+++ prover/src/nodes/downcast.rs
@@ -1,13 +1,5 @@
 use hcs_common::{BMMNode, NodeOpsNative, RequantizeBMMFloatNode, Poly, ReLUNode, ReshapeNode};
 
-#[cfg(feature = "test-types")]
-use {
-    hcs_common::Tensor,
-    ark_bn254::Fr,
-    hcs_common::Ligero,
-    ark_crypto_primitives::sponge::poseidon::PoseidonSponge,
-};
-
 use crate::NodeOpsProve;
 use ark_crypto_primitives::sponge::{Absorb, CryptographicSponge};
 use ark_ff::PrimeField;
@@ -50,27 +42,37 @@ pub fn node_downcast<
         .expect("No implementor of NodeOpsProve was received")
 }
 
-#[test]
+#[cfg(feature = "test-types")]
+mod test {
+    use super::*;
+    use {
+        hcs_common::Tensor,
+        ark_bn254::Fr,
+        hcs_common::Ligero,
+        ark_crypto_primitives::sponge::poseidon::PoseidonSponge,
+    };
 
-fn test_downcast() {
-    let nodes: Vec<Box<dyn NodeOpsNative<i8>>> = vec![
-        Box::new(ReLUNode::<i8>::new(1, 1)),
-        Box::new(ReshapeNode::new(vec![1], vec![1])),
-        Box::new(BMMNode::<i8>::new(
-            Tensor::new(vec![1], vec![1, 1]),
-            Tensor::new(vec![1], vec![1]),
-            1,
-        )),
-        Box::new(RequantizeBMMFloatNode::<i8>::new(1, 1.0, 1, 1.0, 1, 1.0, 1)),
-    ];
+    #[test]
+    fn test_downcast() {
+        let nodes: Vec<Box<dyn NodeOpsNative<i8>>> = vec![
+            Box::new(ReLUNode::<i8>::new(1, 1)),
+            Box::new(ReshapeNode::new(vec![1], vec![1])),
+            Box::new(BMMNode::<i8>::new(
+                Tensor::new(vec![1], vec![1, 1]),
+                Tensor::new(vec![1], vec![1]),
+                1,
+            )),
+            Box::new(RequantizeBMMFloatNode::<i8>::new(1, 1.0, 1, 1.0, 1, 1.0, 1)),
+        ];
 
-    let nodes_proof: Vec<&dyn NodeOpsProve<Fr, PoseidonSponge<Fr>, Ligero<Fr>, i8>> = nodes
-        .iter()
-        .map(|x| node_downcast::<Fr, PoseidonSponge<Fr>, Ligero<Fr>>(x.as_ref()))
-        .collect();
+        let nodes_proof: Vec<&dyn NodeOpsProve<Fr, PoseidonSponge<Fr>, Ligero<Fr>, i8>> = nodes
+            .iter()
+            .map(|x| node_downcast::<Fr, PoseidonSponge<Fr>, Ligero<Fr>>(x.as_ref()))
+            .collect();
 
-    assert!(nodes_proof
-        .iter()
-        .zip(nodes.iter())
-        .all(|(x, y)| { x.type_name() == y.type_name() }));
+        assert!(nodes_proof
+            .iter()
+            .zip(nodes.iter())
+            .all(|(x, y)| { x.type_name() == y.type_name() }));
+    }
 }
